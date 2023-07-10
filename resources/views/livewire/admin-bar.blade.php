1f9d1aff3acc59fc30db679860026d84968ac9e9
Louise Devolder <louise.devolder@onlyhumans.com>
2023-07-10 14:33:00 +0000

    slicing admin bar tabs & translatable strings

--- resources/views/livewire/admin-bar.blade.php
+++ resources/views/livewire/admin-bar.blade.php
@@ -1,24 +1,29 @@
-<div style="background-color: aliceblue">
-    <ul>
-        @foreach ($tabs as $tab)
-            <li
-                @class(['active' => $tab->key() === $current])
-                wire:click="changeTab('{{ $tab->key() }}')"
-            >
-                {{ $tab->name() }}
-            </li>
-        @endforeach
-    </ul>
+<div class="fixed right-0 bottom-0 left-0">
+    <div class="rounded-t-lg border border-gray-300">
+        <ul class="flex items-center rounded-t-lg  bg-gray-100 overflow-hidden">
+            @foreach ($tabs as $tab)
+                <li
+                    @class([
+                        'py-3 px-4 text-sm font-medium text-gray-500 hover:text-gray-800 cursor-pointer',
+                        'active bg-white text-primary-600 hover:text-primary-600' => $tab->key() === $current
+                    ])
+                    wire:click="changeTab('{{ $tab->key() }}')"
+                >
+                    {{ $tab->name() }}
+                </li>
+            @endforeach
+        </ul>
 
-    @foreach ($tabs as $tab)
-        <div
-            {{-- Hide using style --}}
-            @style(['display: none' => ($tab->key() !== $current)])
-            wire:key="{{ $tab->key() }}"
-        >
-            <div wire:ignore>
-                {{ $tab->render() }}
+        @foreach ($tabs as $tab)
+            <div
+                {{-- Hide using style --}}
+                @style(['display: none' => ($tab->key() !== $current)])
+                wire:key="{{ $tab->key() }}"
+            >
+                <div wire:ignore class="p-6" style="height: 400px; overflow-y: auto">
+                    {{ $tab->render() }}
+                </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    </div>
 </div>
--- resources/views/livewire/translatable-strings-tab.blade.php
+++ resources/views/livewire/translatable-strings-tab.blade.php
@@ -1,39 +1,134 @@
 <div>
-    <input type="text" wire:model.debounce.1000ms="query">
+    <div class="filament-tables-search-input mb-5">
+        <label class="group relative flex items-center">
+            <span
+                class="
+                    absolute inset-y-0 left-0
+                    flex items-center justify-center h-9 w-9
+                    text-gray-400
+                    group-focus-within:text-primary-500
+                "
+            >
+                <svg
+                    class="w-5 h-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                >
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                </svg>
+            </span>
+            <input
+                type="text"
+                wire:model.debounce.1000ms="query"
+                class="
+                    block h-9 w-full max-w-xs pl-9
+                    placeholder-gray-400
+                    rounded-lg border-gray-300 shadow-sm
+                    outline-none focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500
+                    transition duration-75
+                "
+            >
+        </label>
+    </div>
 
     <div wire:loading>
-        > Loading icon <
+        <div class="flex gap-3 w-full py-4">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-5 animate-spin"
+            >
+                <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+            </svg>
+            loading...
+        </div>
     </div>
 
     <div wire:loading.remove>
         @if ($message)
-            {{ $message }}
+            <div class="flex gap-3 w-full py-4">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="filament-notifications-icon h-6 w-6 text-success-400"
+                >
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                {{ $message }}
+            </div>
         @endif
 
-        <table>
+        <div class="rounded-xl border border-gray-300 bg-white">
+        <table style="width: 100%;">
+            <tr class="bg-gray-500/5">
+                <th class="p-4 text-left text-sm font-medium text-gray-600">Name</th>
+                <th class="p-4 text-left text-sm font-medium text-gray-600">Translation</th>
+            </tr>
             @foreach ($strings as $string)
-                <tr>
-                    <td>
+                <tr class="border-t border-gray-300">
+                    <td class="py-3 px-4">
                         {{ $string->key }}
                     </td>
 
-                    <td>
+                    <td class="py-3 px-4">
                         @if ($string->is_html)
                             <a
+                                class="
+                                    filament-link filament-tables-link-action
+                                    inline-flex items-center justify-center gap-0.5
+                                    font-medium text-sm text-primary-600
+                                    outline-none focus:underline
+                                    hover:underline hover:text-primary-500
+                                "
                                 href="/admin/translatable-strings/{{ $string->id }}/edit?locale=-{{ app()->getLocale() }}-tab"
                                 target="_blank"
                             >
                                 {{ __('filament-admin-bar::translatable-strings-tab.edit in the cms') }}
                             </a>
                         @else
-                            <input type="text" wire:model.defer="fields.{{ $string->key }}">
+                            <input
+                                class="
+                                    filament-forms-input
+                                    block w-full
+                                    rounded-lg shadow-sm border-gray-300
+                                    outline-none
+                                    transition duration-75
+                                    focus:ring-1 focus:ring-inset focus:border-primary-500 focus:ring-primary-500
+                                    disabled:opacity-70
+                                "
+                                type="text"
+                                wire:model.defer="fields.{{ $string->key }}"
+                            >
                         @endif
                     </td>
                 </tr>
             @endforeach
         </table>
+        </div>
 
-        <button wire:click="submit">
+        <button
+            class="
+                filament-button filament-button-size-md filament-page-button-action
+                inline-flex items-center justify-center gap-1
+                py-1 px-4 min-h-[2.25rem] mt-5
+                font-medium text-sm text-white
+                rounded-lg border shadow border-transparent bg-primary-600
+                transition-colors
+                outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset focus:ring-white
+                focus:bg-primary-700 focus:ring-offset-primary-700
+                hover:bg-primary-500
+            "
+            wire:click="submit"
+        >
             {{ __('filament-admin-bar::translatable-strings-tab.submit') }}
         </button>
     </div>
