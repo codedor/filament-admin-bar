eb2443c6146c5ea3a8916b1d190e0072d62a3f36
AngryMoustache <AngryMoustache@users.noreply.github.com>
2023-07-31 07:14:57 +0000

    Fix styling

--- src/Livewire/TranslatableStringsTab.php
+++ src/Livewire/TranslatableStringsTab.php
@@ -10,7 +10,7 @@ class TranslatableStringsTab extends Component
 {
     public Collection $session;
 
-    public null|string $message = null;
+    public ?string $message = null;
 
     public array $fields = [];
 
