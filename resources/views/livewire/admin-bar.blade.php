21d47ea763e06eec1f1b67a97f4874957523d5d1
Sander Van Damme <sander.vandamme@onlyhumans.com>
2023-10-02 08:58:35 +0000

    WIP styling

--- resources/views/livewire/admin-bar.blade.php
+++ resources/views/livewire/admin-bar.blade.php
@@ -1,22 +1,34 @@
 <div class="filament-admin-bar">
-    <div x-data="{ open: false }" class="fixed right-0 bottom-0 left-0">
+    {!! (new \Filament\Support\Assets\AssetManager)->renderStyles() !!}
+    <link rel="stylesheet" href="{{ asset('css/codedor/filament-admin-bar/assets/filament-admin-bar.css') }}">
+
+    <div
+        class="fixed right-0 bottom-0 left-0"
+        x-data="{
+            open: window.localStorage.getItem('filament-admin-bar-open') === 'true' || false,
+            toggle () {
+                this.open = ! this.open
+                window.localStorage.setItem('filament-admin-bar-open', this.open)
+            }
+        }"
+    >
         <div class="flex justify-end mr-3 mb-3">
             <button
                 type="button"
-                x-on:click="open = !open"
+                x-on:click="toggle()"
                 title="Toggle admin bar"
                 class="
-                    filament-button
                     absolute bottom-full
                     inline-flex items-center justify-center p-3
                     font-medium text-sm text-white
-                    rounded-full border shadow-lg shadow-primary-500/50 bg-primary-600 border-transparent
+                    rounded-full border shadow-lg shadow-teal-500/50 bg-teal-600 border-transparent
                     transition-colors
-                    hover:bg-primary-500
-                    outline-none focus:ring-offset-2 focus-ring-2 focus:ring-inset focus:ring-white focus-bg-primary-700 focus:ring-offset-primary-700
+                    hover:bg-teal-500
+                    outline-none focus:ring-offset-2 focus-ring-2 focus:ring-inset focus:ring-white focus-bg-teal-700 focus:ring-offset-teal-700
                 "
             >
-                <svg
+                <x-heroicon-o-cog class="w-6 h-6" />
+                {{-- <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
@@ -25,23 +37,24 @@
                     class="w-6 h-6"
                 >
                     <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
-                </svg>
+                </svg> --}}
             </button>
         </div>
 
         <div
             x-show="open"
             x-collapse.duration.500ms
+            x-cloak
             class="rounded-t-lg border border-gray-300 bg-white"
         >
             <ul class="flex items-center rounded-t-lg  bg-gray-100 overflow-hidden">
                 @foreach ($tabs as $tab)
                     <li
+                        wire:click="changeTab('{{ $tab->key() }}')"
                         @class([
                             'py-3 px-4 text-sm font-medium text-gray-500 hover:text-gray-800 cursor-pointer',
-                            'active bg-white rounded-t-lg text-primary-600 hover:text-primary-600' => $tab->key() === $current
+                            'active bg-white rounded-t-lg text-teal-600 hover:text-teal-600' => $tab->key() === $current
                         ])
-                        wire:click="changeTab('{{ $tab->key() }}')"
                     >
                         {{ $tab->name() }}
                     </li>
@@ -61,6 +74,4 @@
             @endforeach
         </div>
     </div>
-    {{-- <script src="{{ asset('vendor/filament-admin-bar/assets/app.js') }}"></script> --}}
-    <link rel="stylesheet" href="{{ asset('vendor/filament-admin-bar/assets/filament-admin-bar.css') }}">
 </div>
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [
     laravel({
       publicDirectory: 'resources',
-      buildDirectory: 'dist',
+      // buildDirectory: 'dist',
+      buildDirectory: '../../../../public/css/codedor/filament-admin-bar',
       input: [
         'resources/css/filament-admin-bar.css',
       ],
